4d8b4c063fb2273970fbdfaf77a28b6fe40ee6b0
Jesse Huang <jesse.huang1205@gmail.com>
2023-10-13 18:49:15 -0400

    added namespace and fixed forms

--- CheckOut/CheckOut/urls.py
+++ CheckOut/CheckOut/urls.py
@@ -19,11 +19,10 @@ from django.urls import path, include
 from user import views as user_view
 from django.contrib.auth import views as auth
 
+app_name = 'checkout'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
-    path('login/', user_view.Login, name ='login'),
-    path('logout/', auth.LogoutView.as_view(template_name ='user/index.html'), name ='logout'),
-    path('register/', user_view.register, name ='register'),
+	path('logout/', auth.LogoutView.as_view(template_name ='user/index.html'), name ='logout'),
     path('libraries/', include('libraries.urls')),
 ]
--- CheckOut/user/forms.py
+++ CheckOut/user/forms.py
@@ -8,7 +8,7 @@ class UserRegisterForm(UserCreationForm):
 	first_name = forms.CharField(max_length = 20)
 	class Meta:
 		model = User
-		fields = ['username', 'email', 'password1', 'password2']
+		fields = ['username','first_name', 'email', 'password1', 'password2']
 	def clean_password1(self):
 		password = self.cleaned_data.get("password1")
 		if password.isnumeric():
