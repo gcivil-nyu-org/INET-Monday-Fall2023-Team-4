909866b187581b96cb82d102548e25cb4567afce
Jesse Huang <jesse.huang1205@gmail.com>
2023-10-12 21:59:41 -0400

    template pages
    templates for view library and login that connect to it

--- CheckOut/CheckOut/settings.py
+++ CheckOut/CheckOut/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'login.apps.LoginConfig',
+	'libraries.apps.LibrariesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- CheckOut/CheckOut/urls.py
+++ CheckOut/CheckOut/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
+	path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+	path('libraries/', include('libraries.urls')),
+	path('login/',include('login.urls')),
 ]
