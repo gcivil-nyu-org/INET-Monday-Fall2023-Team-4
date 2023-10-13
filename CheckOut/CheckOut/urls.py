a437f874cce5f68c611bb2bd5650f30824dddf69
Jesse Huang <jesse.huang1205@gmail.com>
2023-10-13 00:58:16 -0400

    linked the libraries page to the home page

--- CheckOut/CheckOut/urls.py
+++ CheckOut/CheckOut/urls.py
@@ -20,9 +20,7 @@ from user import views as user_view
 from django.contrib.auth import views as auth
 
 urlpatterns = [
-	path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-
     path('', include('user.urls')),
     path('login/', user_view.Login, name ='login'),
     path('logout/', auth.LogoutView.as_view(template_name ='user/index.html'), name ='logout'),
