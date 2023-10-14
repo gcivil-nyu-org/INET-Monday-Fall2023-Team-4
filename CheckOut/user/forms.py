f31f2a08e7e2b12d87687ebfc333d244d5ab7abe
xxzz-tt <tanxuanzhi@gmail.com>
2023-10-14 08:30:57 -0400

    refactor sign up

--- CheckOut/user/forms.py
+++ CheckOut/user/forms.py
@@ -4,16 +4,21 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
-	email = forms.EmailField()
-	first_name = forms.CharField(max_length = 20)
+    first_name = forms.CharField(max_length = 20, required=True)
+    last_name = forms.CharField(max_length = 20, required=True)
+    username = forms.CharField(max_length = 20, required=True)
+	email = forms.EmailField(required=True)
+	password1 = forms.CharField(max_length = 20, required=True)
+	password2 = forms.CharField(max_length = 20, required=True)
+ 
 	class Meta:
 		model = User
-		fields = ['username','first_name', 'email', 'password1', 'password2']
+		fields = ['username','first_name', 'last_name' 'email', 'password1', 'password2']
 	def clean_password1(self):
 		password = self.cleaned_data.get("password1")
 		if password.isnumeric():
-			raise forms.ValidationError("Password can not only contain numbers")
+			raise forms.ValidationError("Password cannot only contain numbers")
 		elif password.isalpha():
-			raise forms.ValidationError("Password can not only contain characters")
+			raise forms.ValidationError("Password cannot only contain characters")
 		else:
 			return password
