bd36552d7876f650c4fd4c0206532f25934f7205
Jesse Huang <jesse.huang1205@gmail.com>
2023-10-18 17:20:33 -0400

    Email verification
    email verification code sent

--- CheckOut/user/views.py
+++ CheckOut/user/views.py
@@ -5,12 +5,15 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import PasswordChangeView
 from django.contrib.messages.views import SuccessMessageMixin
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, ValidateForm
 from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.template import Context
 from django.urls import reverse_lazy
+from smtplib import SMTP, SMTPException
+import time
+import random,string
 
 from .forms import UserRegisterForm, UpdateUserForm
 from django.views import View
@@ -19,31 +22,78 @@ from django.views import View
 def index(request):
 	return render(request, 'user/index.html', {'title':'index'})
 
-class RegisterView(View):
-    form_class = UserRegisterForm
-    initial = {'key': 'value'}
-    template = 'user/register.html'
-    
-    def get(self, request, *args, **kwargs):
-        form = self.form_class(initial=self.initial)
-        return render(request, self.template, {'form': form})
-
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
-
-        if form.is_valid():
-            form.save()
-
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username,password=password)
-            login(request, user)
-   
-            messages.success(request, f'Account created for {username}')
-
-            return redirect(to='/')
-
-        return render(request, self.template, {'form': form})
+def register(request):
+	if request.method == 'POST':
+		if 'signup' in request.POST:
+			form = UserRegisterForm(request.POST)
+			request.session['register_form'] = request.POST
+			if form.is_valid():
+				vcode = ''.join(random.choices(string.ascii_letters, k=5))
+				email = form.cleaned_data.get('email');
+				print(f'vcode: {vcode}')
+				request.session['verification_code'] = { 'code': vcode , 'ttl' : (time.time() + 300)}
+				subject, from_email,message = 'Verify Your Email', 'test@gmail.com', f'This is your verification code: {vcode}'
+				try:
+					send_mail(subject,message,from_email,[email],fail_silently = False,)
+					context = {
+						'form' : form,
+						'title' : 'validate code',
+						'verify_code': True,
+					}
+					return render(request,'user/register.html',context)
+				except SMTPException as e:
+					print(e)
+					context = {
+						'form' : form,
+						'title' : 'validate code',
+						'verify_code': True,
+						'email_send_failed' : True
+					}
+					return render(request,'user/register.html',context)
+		elif 'verify' in request.POST:
+			print('Verify stuff')
+			validate_form = ValidateForm(request.POST)
+			print(request.POST)
+			if validate_form.is_valid():
+				code = validate_form.cleaned_data.get('code')
+				validation_token = request.session.get('verification_code')
+				print(f'{validation_token}\n Time Now: {time.time()}')
+				if validation_token and validation_token['ttl'] > time.time():
+					if code == validation_token['code'] :
+						post_req = request.session.pop('register_form')
+						form = UserRegisterForm(post_req)
+						form.save()
+						username = form.cleaned_data.get('username')
+						email = form.cleaned_data.get('email')
+						password = form.cleaned_data.get('password1')
+							
+						user = authenticate(username=username,password=password)
+						login(request,user)
+						messages.success(request, f'Your account has been created ! You are now able to log in')
+						del request.session['verification_code']
+						return redirect('users:index')
+					else:
+						context = {
+							'title' : 'validate code',
+							'verify_code': True,
+							'validate_failed': True,
+							'error_text': 'Incorrect Validation Code, make sure you entered in the right code.'
+						}
+						return render(request,'user/register.html',context)
+				else:
+					context = {
+							'title' : 'validate code',
+							'verify_code': True,
+							'validate_failed': True,
+							'error_text': 'The verification code has expired.'
+					}
+					return render(request,'user/register.html',context)
+			else:
+				print("Failed validation")
+			
+	else:
+		form = UserRegisterForm()
+	return render(request, 'user/register.html', {'form': form, 'title':'register here'})
 
 def Login(request):
 	if request.method == 'POST':
