687f07bcfed40a176e70c40f8cd9f36c14e1e4f6
Jesse Huang <jesse.huang1205@gmail.com>
2023-10-29 23:53:19 -0400

    tests for login view

--- user/functional_tests/tests.py
+++ user/functional_tests/tests.py
@@ -2,6 +2,42 @@ from django.test import TestCase
 from user.forms import UserRegisterForm
 from django.urls import reverse
 
+class LoginViewTest(TestCase):
+    def setUp(self):
+        formdata = {
+            "first_name": "test3first",
+            "last_name": "test3last",
+            "password1": "testP@ssword1",
+            "password2": "testP@ssword1",
+            "username": "test3",
+            "email": "test3@email.com",
+        }
+        self.testvalidformdata = formdata
+        self.testvalidform = UserRegisterForm(data=formdata)
+
+    def test_invalid_credentials(self):
+        formdata = {
+            'username': 'invalid',
+            'password': 'invalid',
+        }
+        response = self.client.post(reverse('users:login'),formdata)
+        msgs = list(response.context['messages'])
+        self.assertEqual(str(msgs[0]),'Account does not exist. Please sign up.')
+
+    def test_valid_credentials(self):
+        formdata = {
+            'username': 'test3',
+            'password': 'testP@ssword1',
+        }
+        postdata = {"signup": ""}
+        postdata.update(self.testvalidformdata)
+        response = self.client.post(reverse("users:register"), postdata)
+        code = self.client.session.get("verification_code")["code"]
+        postdata = {"verify": "", "code": code}
+        response = self.client.post(reverse("users:register"), postdata)
+        response = self.client.post(reverse('users:login'),formdata)
+        self.assertRedirects(response,reverse('users:index'))
+
 
 class UserViewTest(TestCase):
     def setUp(self):
