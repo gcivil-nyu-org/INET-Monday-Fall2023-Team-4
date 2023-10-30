2c95cd5ae5832245ed833f1a9ab956074dc271bb
Zoila Rodriguez <zrodriguez@colgate.edu>
2023-10-29 23:06:52 -0400

    Update tests.py

--- libraries/tests.py
+++ libraries/tests.py
@@ -1,3 +1,88 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.urls import reverse
+from django.template.loader import get_template
+from django.http import HttpRequest
+from django.test import Client
+from django.test import RequestFactory
+from django.http import HttpResponse
+from django.template import Template, Context
+from django.template.loader import get_template
 
-# Create your tests here.
+from libraries.models import Library
+from libraries.views import index, LibraryListView, LibraryDetailView
+
+c = Client()
+
+class IndexViewTest(TestCase):
+    def test_index_view(self):
+        factory = RequestFactory()
+        request = factory.get('/')
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
+        expected_template = get_template('libraries/index.html') 
+        self.assertEqual(response.content, expected_template.render({}).encode()) 
+        
+class LibrariesTestCase(TestCase):
+    def setUp(self):
+        Library.objects.create(
+            id=1, 
+            branch="Library Test Case Branch", 
+            address="123 Test Unit Drive", 
+            city="Coveralls", 
+            postcode="65432", 
+            phone="(123)456-7890", 
+            monday="9:00AM - 5:00PM", 
+            tuesday="9:00AM - 5:00PM", 
+            wednesday="9:00AM - 5:00PM", 
+            thursday="9:00AM - 5:00PM", 
+            friday="9:00AM - 5:00PM", 
+            saturday="9:00AM - 5:00PM", 
+            sunday="9:00AM - 5:00PM", 
+            latitude=0.0, 
+            longitude=0.0, 
+            link="https://github.com/gcivil-nyu-org/INET-Monday-Fall2023-Team-4", 
+            NYU=1)
+        Library.objects.create(
+            id=2, 
+            branch="Library Unit Test Branch", 
+            address="123 Test Unit Drive", 
+            city="Coveralls", 
+            postcode="65432", 
+            phone="(123)456-7890", 
+            monday="9:00AM - 5:00PM", 
+            tuesday="9:00AM - 5:00PM", 
+            wednesday="9:00AM - 5:00PM", 
+            thursday="9:00AM - 5:00PM", 
+            friday="9:00AM - 5:00PM", 
+            saturday="9:00AM - 5:00PM", 
+            sunday="9:00AM - 5:00PM", 
+            latitude=0.0, 
+            longitude=0.0, 
+            link="https://github.com/gcivil-nyu-org/INET-Monday-Fall2023-Team-4", 
+            NYU=0)
+        
+    def test_library_ordering(self):
+        branch1 = Library.objects.get(id=1)
+        ordering = Library._meta.ordering
+        self.assertEquals(ordering[0], "-branch")
+
+    def test_library_information(self):
+        branch1 = Library.objects.get(id=1)
+        branch2 = Library.objects.get(id=2)
+        self.assertEqual(getattr(branch1, "branch"), "Library Test Case Branch")
+        self.assertTrue(getattr(branch2, "branch"), "Library Unit Test Branch")
+
+    # def test_get_today_hours(self):
+    #     days_in_week = [
+    #         self.monday,
+    #         self.tuesday,
+    #         self.wednesday,
+    #         self.thursday,
+    #         self.friday,
+    #         self.saturday,
+    #         self.sunday,
+    #     ]
+    #     current_time = datetime.now()
+    #     weekday = current_time.weekday()
+    #     result = Library.get_today_hours(self)
+    #     self.assertEqual(weekday.lower(), result)
