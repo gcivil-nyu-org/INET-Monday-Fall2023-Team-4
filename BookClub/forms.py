4d311d7d063fc920c4e63e013940911e290dfdc7
rg4352 <rg4352@nyu.edu>
2023-11-02 21:50:25 -0400

    Remove case-insensitive directories from index

--- BookClub/forms.py
+++
@@ -1,7 +0,0 @@
-# from django.forms import ModelForm
-# from .models import BookClub
-
-# class BookClubForm(ModelForm):
-#     class Meta:
-#         model = BookClub
-#         fields = ["name","description","currentBook","meetingSchedule",]
--- BookClub/urls.py
+++
@@ -1,7 +0,0 @@
-# from django.urls import path
-
-# from . import views
-
-# urlpatterns = [
-#     path("create/<int:library_id>/", views.create_book_club, name="bookClub"),
-# ]
