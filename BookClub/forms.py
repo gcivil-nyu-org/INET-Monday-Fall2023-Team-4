e084b8e081d8d426fc4925ed4fe4b3674f69d836
rg4352 <rg4352@nyu.edu>
2023-11-02 22:46:00 -0400

    adding forms

--- BookClub/forms.py
+++ BookClub/forms.py
@@ -2,8 +2,16 @@ from django import forms
 from django.forms import ModelForm
 from .models import BookClub
 
+
 class BookClubForm(ModelForm):
     class Meta:
         model = BookClub
-        
+        fields = (
+            "name",
+            "description",
+            "currentBook",
+            "meetingDay",
+            "meetingStartTime",
+            "meetingEndTime",
+            "meetingOccurence")
 
