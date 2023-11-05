582e40364ad713cc5c63c47e553088a20d044290
xxzz-tt <tanxuanzhi@gmail.com>
2023-11-04 21:52:23 -0400

    edits

--- Subscription/urls.py
+++ Subscription/urls.py
@@ -1,12 +1,5 @@
-from django.urls import path
-from Subscription.views import toggle_club_join
+# from django.urls import path
 
 app_name = "Subscription"
 
-urlpatterns = [
-    path(
-        "toggle-join/<int:user_pk>/<int:bookclub_pk>/",
-        toggle_club_join,
-        name="toggle-join",
-    ),
-]
+urlpatterns = []
