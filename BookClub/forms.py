7c8583e87212cf2a8e3aa6beef8eebbeaa615f2e
Jesse Huang <jesse.huang1205@gmail.com>
2023-11-05 12:50:25 -0500

    some more changes that didnt merge when branching of master

--- BookClub/forms.py
+++ BookClub/forms.py
@@ -1,7 +1,16 @@
-# from django.forms import ModelForm
-# from .models import BookClub
+from django.forms import ModelForm
+from .models import BookClub
 
-# class BookClubForm(ModelForm):
-#     class Meta:
-#         model = BookClub
-#         fields = ["name","description","currentBook","meetingSchedule",]
+
+class BookClubForm(ModelForm):
+    class Meta:
+        model = BookClub
+        fields = (
+            "name",
+            "description",
+            "currentBook",
+            "meetingDay",
+            "meetingStartTime",
+            "meetingEndTime",
+            "meetingOccurence",
+        )
