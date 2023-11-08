a88baa88e4becdaa324a5e960cb9498af47f71ec
Jesse Huang <jesse.huang1205@gmail.com>
2023-11-08 17:59:14 -0500

    create book club page

--- BookClub/urls.py
+++ BookClub/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import create_book_club
+from .views import create_book_club,book_club_details
 
+appname="bookclub"
 urlpatterns = [
     path("create/", create_book_club, name="create-book-club"),
+	path("details/<slug:slug>/",book_club_details,name="details")
 ]
--- BookClub/views.py
+++ BookClub/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 from .forms import BookClubForm
+from django.conf import settings as conf_settings
+from .models import BookClub
+from libraries.models import Library
+from django.contrib import messages
 
+
+def book_club_details(request,slug):
+    bc = BookClub.objects.get(id=slug)
+    context = {
+        'key' :conf_settings.GOOGLE_API_KEY,
+		'bookclub': bc,
+		'member_count': bc.members.all().count(),
+		'lat' : 40.660152,
+		'lng' : -73.739488
+    }
+    if request.method == "POST":
+        if 'subscribe' in request.POST:
+            if (not bc.libraryId.NYU) or (bc.libraryId.NYU and request.user.status == 'nyu'):
+                bc.members.add(request.user)
+            else:
+                messages.error(
+                request,
+                "You must be a NYU student to subscribe to this book club"
+            )
+	
+    return render(request,'details.html',context)
 
 def create_book_club(request):
     form = BookClubForm
