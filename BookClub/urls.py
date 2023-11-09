2006191e82dc9c4e4a97ae1c5366f360987ccedb
Jesse Huang <jesse.huang1205@gmail.com>
2023-11-09 09:33:04 -0500

    black formatting changes

--- BookClub/urls.py
+++ BookClub/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import create_book_club,book_club_details
+from .views import create_book_club, book_club_details
 
 app_name = "bookclub"
 urlpatterns = [
     path("create/", create_book_club, name="create-book-club"),
-	path("details/<slug:slug>/",book_club_details,name="details")
+    path("details/<slug:slug>/", book_club_details, name="details"),
 ]
--- BookClub/views.py
+++ BookClub/views.py
@@ -1,32 +1,34 @@
 from django.shortcuts import render, redirect
 from .forms import BookClubForm
 from .models import BookClub
-from libraries.models import Library
 from django.contrib import messages
 
 
-def book_club_details(request,slug):
+def book_club_details(request, slug):
     bc = BookClub.objects.get(id=slug)
     context = {
-		'bookclub': bc,
-		'member_count': bc.members.all().count(),
-		'subscribed': bc.members.contains(request.user) if request.user.is_authenticated else False
+        "bookclub": bc,
+        "member_count": bc.members.all().count(),
+        "subscribed": bc.members.contains(request.user)
+        if request.user.is_authenticated
+        else False,
     }
-    print(context)
     if request.method == "POST":
-        if 'subscribe' in request.POST:
-            if (bc.libraryId.NYU == '0') or (bc.libraryId.NYU == '1' and request.user.status == 'nyu'):
+        if "subscribe" in request.POST:
+            if (bc.libraryId.NYU == "0") or (
+                bc.libraryId.NYU == "1" and request.user.status == "nyu"
+            ):
                 bc.members.add(request.user)
             else:
                 messages.error(
-                request,
-                "You must be a NYU student to subscribe to this book club"
-            )
-            return redirect("bookclub:details",slug=slug)
-        elif 'unsubscribe' in request.POST:
+                    request, "You must be a NYU student to subscribe to this book club"
+                )
+            return redirect("bookclub:details", slug=slug)
+        elif "unsubscribe" in request.POST:
             bc.members.remove(request.user)
-            return redirect("bookclub:details",slug=slug)
-    return render(request,'details.html',context)
+            return redirect("bookclub:details", slug=slug)
+    return render(request, "details.html", context)
+
 
 def create_book_club(request):
     form = BookClubForm
