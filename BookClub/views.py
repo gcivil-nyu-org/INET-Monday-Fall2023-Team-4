ed1c35f9fa25e1c0ea64ef6775133f1fcbe5365c
rg4352 <rg4352@nyu.edu>
2023-11-10 00:35:13 -0500

    button, book club, nyu logic

--- BookClub/views.py
+++ BookClub/views.py
@@ -1,21 +1,38 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from .forms import BookClubForm
 from .models import BookClub
 from .models import Library
 from django.contrib.auth.decorators import login_required
-
+from django.contrib import messages
 
 @login_required
 def create_book_club(request):
+    library_id = request.GET.get('libraryId') or request.POST.get('libraryId')
+    library = Library.objects.get(id=int(library_id))
+    form = BookClubForm(request.POST or None) 
     if request.method == "POST":
-        form = BookClubForm(request.POST)
-        if form.is_valid():
-            book_club = form.save(commit=False)
-            book_club.admin = request.user
-            book_club.libraryId = Library.objects.get(id=4)
-            book_club.save()
-            book_club.members.add(request.user)
+        if request.user.status == "nyu" and library.NYU == "1":
+            if form.is_valid():
+                book_club = form.save(commit=False)
+                book_club.admin = request.user
+                book_club.libraryId = library
+                book_club.save()
+                book_club.members.add(request.user)
+                library2 = get_object_or_404(Library, pk=library_id)
+                return render(request, "libraries/library_detail.html",{"library": library2})
+        elif request.user.status != "nyu" and library.NYU == "1":
+            messages.error(request, "You are not allowed to create a book club for NYU libraries.")
 
+        else:
+            if form.is_valid():
+                library = Library.objects.get(id=int(library_id))    
+                book_club = form.save(commit=False)
+                book_club.admin = request.user
+                book_club.libraryId = library
+                book_club.save()
+                book_club.members.add(request.user)
+                library2 = get_object_or_404(Library, pk=library_id)
+                return render(request, "libraries/library_detail.html",{"library": library2})
     else:
         form = BookClubForm()
     return render(request, "bookclub.html", {"form": form})
--- user/models.py
+++ user/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+
 class CustomUser(AbstractUser):
     STATUS = (
         ("reader", "Reader"),
