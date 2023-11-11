6e79964c3f7d187e2559a2f214184c74abf95d8d
rg4352 <rg4352@nyu.edu>
2023-11-11 15:18:38 -0500

    centering of bookclub in lib details

--- BookClub/views.py
+++ BookClub/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from libraries.models import Library
-from .forms import BookClubForm, BookClubEditForm
 from .models import BookClub
 from django.contrib import messages
 from django.http import HttpResponseForbidden
