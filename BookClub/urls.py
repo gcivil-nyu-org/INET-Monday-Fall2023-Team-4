cbb1fc0c52fdd41a2edff3e4794eb68ad7a5ff1a
rg4352 <rg4352@nyu.edu>
2023-11-11 15:10:28 -0500

    import errors fixed

--- BookClub/urls.py
+++ BookClub/urls.py
@@ -3,8 +3,8 @@ from .views import create_book_club, edit_book_club, book_club_detail
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path("create/", create_book_club, name="create-book-club"),
-    path("details/<slug:slug>/", book_club_details, name="details"),
+    path("create/", login_required(create_book_club), name="create-book-club"),
+    path("details/<slug:slug>/", book_club_detail, name="details"),
     path("<int:book_club_id>/edit/", edit_book_club, name="edit_book_club"),
     path("<int:book_club_id>/", book_club_detail, name="book_club_detail"),
 ]
--- BookClub/views.py
+++ BookClub/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from libraries.models import Library
 from .forms import BookClubForm, BookClubEditForm
 from .models import BookClub
 from django.contrib import messages
@@ -8,6 +9,7 @@ from .forms import BookClubForm, BookClubEditForm
 from django.conf import settings
 from django.core.mail import send_mail
 from smtplib import SMTPException
+from django.contrib.auth.decorators import login_required
 
 
 def book_club_details(request, slug):
