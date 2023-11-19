cdad7a4a769951af6355fcc9f48f42bf0d9b55b0
FranckyXue <xyk.francky@gmail.com>
2023-11-18 21:56:28 -0500

    first

--- BookClub/urls.py
+++ BookClub/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import include,path
 from .views import create_book_club, edit_book_club, book_club_details
 from django.contrib.auth.decorators import login_required
+
 
 urlpatterns = [
     path("create/", login_required(create_book_club), name="create-book-club"),
--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
-
 def index(request):
-    return render(request, "chat/index.html")
+    return render(request, 'chat/index.html')
 
+# def room(request, room_name):
+#     return render(request, 'chat/room.html', {
+#         'room_name': room_name
+#     })
 
 def room(request, room_name):
-    return render(request, "chat/room.html", {"room_name": room_name})
+    return render(request, 'chat/room.html', {
+        'room_name': room_name, 'username': request.user.username
+    })
+
+
+def name(request, roomname):
+    # Do any processing if needed, then pass the roomname to the template
+    return render(request, 'chat/name.html', {'roomname': roomname})
