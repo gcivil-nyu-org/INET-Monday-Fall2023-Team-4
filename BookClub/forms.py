130a7ede9ef2c697194a9794d9bdd81af500a427
rg4352 <rg4352@nyu.edu>
2023-11-24 15:18:32 -0500

    fixed bugs

--- BookClub/forms.py
+++ BookClub/forms.py
@@ -1,5 +1,7 @@
 from django.forms import ModelForm
 from .models import BookClub
+from django.core.exceptions import ValidationError
+from django import forms
 
 
 class BookClubForm(ModelForm):
@@ -14,8 +16,28 @@ class BookClubForm(ModelForm):
             "meetingEndTime",
             "meetingOccurence",
         )
+        labels = {
+            'currentBook': 'Current Book',
+            'meetingStartTime': 'Meeting Start Time',
+            'meetingEndTime': 'Meeting End Time',
+            "meetingDay": "Meeting Day",
+        }
+    def __init__(self, *args, **kwargs):
+        super(BookClubForm, self).__init__(*args, **kwargs)
+        self.fields['meetingDay'].required = True
+        self.fields['meetingOccurence'].required = True
+        self.fields['meetingStartTime'].widget = forms.TimeInput(format='%I:%M %p', attrs={'type': 'time'})
+        self.fields['meetingEndTime'].widget = forms.TimeInput(format='%I:%M %p', attrs={'type': 'time'})
+    def clean(self):
+        cleaned_data = super().clean()
+        start_time = cleaned_data.get("meetingStartTime")
+        end_time = cleaned_data.get("meetingEndTime")
 
+        if start_time and end_time and end_time <= start_time:
+            raise ValidationError("Meeting end time must be after the meeting start time.")
 
+        return cleaned_data
+    
 class BookClubEditForm(ModelForm):
     class Meta:
         model = BookClub
