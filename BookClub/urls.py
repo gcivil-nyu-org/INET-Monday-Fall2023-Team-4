ae8b99a9dcb97696e4cd2439cfd5ee05c55c9746
rg4352 <rg4352@nyu.edu>
2023-12-01 15:30:52 -0500

    delete book club functionality

--- BookClub/urls.py
+++ BookClub/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
-from .views import create_book_club, edit_book_club, book_club_details, error_page
+from .views import (
+    create_book_club,
+    edit_book_club,
+    book_club_details,
+    error_page,
+    delete_book_club,
+    deletion_confirmation,
+)
 from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    path("create", login_required(create_book_club), name="create-book-club"),
     path("error", error_page, name="error_page"),
+    path("create", login_required(create_book_club), name="create-book-club"),
+    path("delete", login_required(delete_book_club), name="delete_book_club"),
+    path("delete/confirmation", deletion_confirmation, name="deletion_confirmation"),
     path("<slug:slug>", book_club_details, name="details"),
     path("<int:book_club_id>/edit", edit_book_club, name="edit_book_club"),
 ]
